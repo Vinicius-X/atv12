9c5100b55779d06a0ff8a8e438d45a1d7767aa91
Vinicius <160507786+Vinicius-X@users.noreply.github.com>
2024-03-13 11:56:26 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>teste do xampp</title>
+</head>
+<body>
+    <h1>Página teste html</h1>
+    <p>teste pagina php</p>
+    
+    <?php
+            echo "Olá, eu sou um script PHP!";
+        ?>
+    
+</body>
+</html>
